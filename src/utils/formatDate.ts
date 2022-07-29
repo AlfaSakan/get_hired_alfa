47515f65b47749a8b2d4f241160d22a8f735b2ca
alfa sakan <ahmadsakan@ist.id>
2022-07-29 11:06:59 +0700

    feat: create function sum

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,19 @@
-export const months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+export const months = [
+  "Januari",
+  "Februari",
+  "Maret",
+  "April",
+  "Mei",
+  "Juni",
+  "Juli",
+  "Agustus",
+  "September",
+  "Oktober",
+  "November",
+  "Desember",
+];
 
-export const formatDate = (date: Date) =>  `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+export const formatDate = (date: Date) =>
+  `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+
+export const sum = (num1: number, num2: number) => num1 + num2;
