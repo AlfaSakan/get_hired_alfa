abeee1596aeed5857a59ed41abe16babe4ebfad3
alfa sakan <ahmadsakan@ist.id>
2022-07-29 16:28:20 +0700

    fix: added and rename data-cy

--- src/components/molecules/EditText/EditText.molecule.tsx
+++ src/components/molecules/EditText/EditText.molecule.tsx
@@ -17,29 +17,31 @@ const EditText: React.FC<IProps> = ({ onChange, value, onEdited }) => {
     setIsEditMode(!isEditMode);
   };
 
+  if (isEditMode) {
+    return (
+      <div className="flex items-center" data-cy="todo-title">
+        <input
+          autoFocus
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className="mr-6 bg-inherit focus:outline-none border-b border-black text-4xl font-bold text-dark pb-2"
+        />
+        <div onClick={onIsEditModeHandler} data-cy="todo-title-edit-button">
+          <img src={EditIcon} alt="edit" className="scale-125" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
       className="flex items-center"
       onClick={onIsEditModeHandler}
       data-cy="todo-title"
     >
-      {isEditMode ? (
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          className="mr-6 bg-inherit focus:outline-none border-b border-black text-4xl font-bold text-dark pb-2"
-        />
-      ) : (
-        <h1
-          className="text-4xl font-bold text-dark mr-6"
-          // data-cy="activity-title"
-          // data-cy="todo-title"
-        >
-          {value}
-        </h1>
-      )}
-      <div>
+      <h1 className="text-4xl font-bold text-dark mr-6">{value}</h1>
+      <div data-cy="todo-title-edit-button">
         <img src={EditIcon} alt="edit" className="scale-125" />
       </div>
     </div>
